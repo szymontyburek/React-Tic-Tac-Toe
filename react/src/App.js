0b8198f769150bded153b814c81217afc9eb7f0a
Szymon Tyburek <szymontyburek10@gmail.com>
2024-05-29 17:28:08 -0700

    learing to programmatically count the grid squares

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
-  );
+function Square() {
+  return <button className="square">1</button>;
 }
 
-function MyButton({ count, onClick }) {
-  return <button onClick={onClick}>Clicked {count} times</button>;
+export default function Board() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
